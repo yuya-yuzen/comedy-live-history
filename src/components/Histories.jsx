6a8364045a2dbf45bb1ad7d41c592005bc37dd88
yuya <yuya.kyutech@gmail.com>
2024-07-07 19:16:44 +0900

    feat: 🎸 タイムライン表示を左に寄せる

--- src/components/Histories.jsx
+++ src/components/Histories.jsx
@@ -1,4 +1,5 @@
 import Timeline from "@mui/lab/Timeline";
+import { timelineOppositeContentClasses } from "@mui/lab/TimelineOppositeContent";
 
 import mockData from "../mockData";
 import History from "./History";
@@ -10,7 +11,13 @@ function Histories() {
     <>
       <p>合計 {histories.length} 本</p>
       <p>※新しい順に記載</p>
-      <Timeline>
+      <Timeline
+        sx={{
+          [`& .${timelineOppositeContentClasses.root}`]: {
+            flex: 0.1,
+          },
+        }}
+      >
         {[...histories].reverse().map((history, index) => (
           <History key={index} history={history} />
         ))}

16081ae9190896db61b5829b38cbeef696075967
yuya <yuya.kyutech@gmail.com>
2024-12-29 21:14:45 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "csv"
+
+CSV.foreach("/rails/db/seed.csv", headers: true) do |row|
+  live = Live.find_or_create_by!(name: row['live_name'], date: row['live_date'])
+  next if row['comedian_names'].blank?
+
+  row['comedian_names'].split(',').each do |comedian_name|
+    comedian = Comedian.find_or_create_by!(name: comedian_name)
+    Performer.find_or_create_by!(live: live, comedian: comedian)
+  end
+end
